38505d248ef460002e19b76e6b622ed189515b6d
jude <jude.faustino12@gmail.com>
2025-06-14 14:29:59 +0800

    imported company logo

--- src/components/logo and images/IndraLogo.jsx
+++ src/components/logo and images/IndraLogo.jsx
@@ -1,8 +1,9 @@
+import logo from "/src/assets/AF_INDRA_LOG_RGB_POS.svg";
 function IndraLogo() {
   return (
     <div>
       <img
-        src="/src/assets/AF_INDRA_LOG_RGB_POS.svg"
+        src={logo}
         alt="shows the company logo"
         className="w-72 h-20 p-6 mt-10 text-[#004254] 
         sm:w-[36rem] sm:h-[9rem] sm:p-4
