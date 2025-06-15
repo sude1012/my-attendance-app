6d4f606441c9560cab895116e0ee8a2a019ac563
jude <jude.faustino12@gmail.com>
2025-06-15 15:03:47 +0800

    test2

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,7 @@
-export function handler = async (event, context) => {
+export async function handler(event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello from Netlify Functions!" }),
+    headers: { "Content-Type": "application/json" },
   };
-};
+}
