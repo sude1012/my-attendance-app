f3c08e259c21ecaf066a564d958acef3376647d3
jude <jude.faustino12@gmail.com>
2025-06-15 22:07:23 +0800

    server

--- config/psql.js
+++ config/psql.js
@@ -21,7 +21,11 @@ if (process.env.NODE_ENV === "production") {
 dotenv.config();
 
 const { Pool } = pkg;
-
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "*****" : "NOT SET");
+console.log("DB_PORT:", process.env.DB_PORT);
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
